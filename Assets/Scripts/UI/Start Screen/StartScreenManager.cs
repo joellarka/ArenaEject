88d196ac942c17bb4e6e801b35beb768f88b406e
Ronja <ronja_mattsson@outlook.com>
2024-04-15 11:22:44 +0200

    Start Screen

---
+++ Assets/Scripts/UI/Start Screen/StartScreenManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenManager : MonoBehaviour
+{
+    private static StartScreenManager instance;
+    public static StartScreenManager Instance { get => instance; }
+
+    [SerializeField] private GameObject joinScreen;
+    [SerializeField] private GameObject optionsScreen;
+    [SerializeField] private GameObject creditsScreen;
+    [SerializeField] private List<GameObject> screens;
+
+
+    private void Awake()
+    {
+        if (instance == null || instance == this)
+        {
+            instance = this;
+            screens = new List<GameObject>();
+            if(joinScreen != null) screens.Add(joinScreen);
+            if (optionsScreen != null) screens.Add(optionsScreen);
+            if (creditsScreen != null) screens.Add(creditsScreen);
+
+            TurnOffAllSCreens();
+        }
+        else
+        {
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void GoToJoin()
+    {
+        GoToScreen(joinScreen);
+    }
+
+    public void GoToOptions()
+    {
+        GoToScreen(optionsScreen);
+    }
+    public void GoToCredits()
+    {
+        GoToScreen(creditsScreen);
+    }
+
+    private void GoToScreen(GameObject screen)
+    {
+        TurnOffAllSCreens(screen);
+        if (screen != null) screen.SetActive(!screen.activeSelf);
+    }
+
+    public void GoToDefault()
+    {
+        TurnOffAllSCreens();
+    }
+
+    private void TurnOffAllSCreens()
+    {
+        foreach (GameObject s in screens)
+        {
+            s.SetActive(false);
+        }
+    }
+
+    private void TurnOffAllSCreens(GameObject exclude)
+    {
+        foreach (GameObject s in screens)
+        {
+            if(s != exclude)
+            {
+                s.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Paths.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Paths
+{
+    public const string GAME_SCENE_NAME = "_gameScene";
+}
