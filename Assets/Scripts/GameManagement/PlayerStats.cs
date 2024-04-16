7d488fd368a2a1158b0fa781909adfa5cda6ef66
Ronja <ronja_mattsson@outlook.com>
2024-04-16 15:12:47 +0200

    players now spawn with different colors

--- Assets/Scripts/GameManagement/PlayerStats.cs
+++ Assets/Scripts/GameManagement/PlayerStats.cs
@@ -7,4 +7,19 @@ public class PlayerStats : MonoBehaviour
     public int playerIndex = 1;
     public int lives = 3;
     public bool alive { get => lives > 0; }
+
+    [Header("TEMP")]
+    public List<Color> colors = new List<Color>();
+    public MeshRenderer myRenderer;
+
+    private void Start()
+    {
+        if(myRenderer != null)
+        {
+            if (colors.Count != 4) return;
+
+            myRenderer.material = Instantiate(myRenderer.material);
+            myRenderer.material.color = colors[playerIndex-1];
+        }
+    }
 }
