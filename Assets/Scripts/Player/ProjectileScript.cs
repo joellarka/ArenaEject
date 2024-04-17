819e22bd1be55a566ee655ef4051a09da8e491d8
Pirat1001 <92086882+Pirat1001@users.noreply.github.com>
2024-04-17 10:03:20 +0200

    Adding player friction

--- Assets/Scripts/Player/ProjectileScript.cs
+++ Assets/Scripts/Player/ProjectileScript.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float destroyDelay = 3f;
-    public float knockbackForce = 5f;
+    public float knockbackForce = 15f;
     public float knockbackDuration = 0.5f;
-    public float frictionCoefficient = 10f; // Adjust this value to control the friction effect
 
-    private bool isKnockbackApplied = false;
     private Rigidbody playerRb; // Reference to the player's rigidbody
-    private PlayerFriction friction;
 
     void Start()
     {
@@ -32,20 +29,9 @@ public class Projectile : MonoBehaviour
         if (collision.gameObject.CompareTag("Player") && playerRb == null)
         {
             playerRb = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 knockbackDirection = -collision.contacts[0].normal;
+            Vector3 knockbackDirection = collision.contacts[0].normal;
             playerRb.AddForce(knockbackDirection * knockbackForce, ForceMode.Impulse);
-            friction = collision.gameObject.GetComponent<PlayerFriction>();
         }
-    }
-
-    void FixedUpdate()
-    {
-        if(playerRb != null)
-        {
-            if(friction != null && playerRb.velocity.magnitude > 0)
-            {
-                friction.DeacceleratePlayer();
-            }
-        }
+        Destroy(gameObject);
     }
 }
